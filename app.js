f443fb41c015ac6c3d18110a39b8c075fcf5e365
Brad Smith <bradsmith712@gmail.com>
2018-06-15 11:08:35 -0500

    test

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const layout = require("./views/layout.js")
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const layout = require('./views/layout.js');
 
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(express.static(__dirname + '/public'));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(bodyParser.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-    res.send(layout());
+  res.send(layout());
 });
+
+console.log('hello');
 
 app.listen(3000);
