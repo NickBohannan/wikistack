241957c8bb61ce0ef336c0ee3048359eb7cf7f7b
Nick Bohannan <nbohannan@gmail.com>
2018-06-15 10:54:39 -0500

    first commit to Brad

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const layout = require("./views/layout.js")
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+
+app.get('/', (req, res) => {
+    res.send(layout());
+});
+
+app.listen(3000);
